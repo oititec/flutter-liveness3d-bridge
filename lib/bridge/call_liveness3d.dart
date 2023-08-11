716d8969d05b2a582527e04926e3ec5f7475a5c3
Vitor Souza <vitorx1280@gmail.com>
2023-08-11 15:29:02 -0300

    fix: replace the liveness 3D success result parse

--- lib/bridge/call_liveness3d.dart
+++ lib/bridge/call_liveness3d.dart
@@ -5,7 +5,31 @@ class OitiLiveness3d {
     String appKey,
     bool isProd,
   ) async {
-    return await OitiLiveness3dPlatform.instance
-        .startLiveness3d(appKey, isProd);
+    final result =
+        await OitiLiveness3dPlatform.instance.startLiveness3d(appKey, isProd);
+
+    return LivenessSuccessResult(
+      result['valid'] ?? false,
+      result['cause'] ?? '',
+      result['codId'] ?? '',
+      result['protocol'] ?? '',
+      result['scanResultBlob'] ?? '',
+    );
   }
+}
+
+class LivenessSuccessResult {
+  final bool valid;
+  final String cause;
+  final String codId;
+  final String protocol;
+  final String scanResultBlob;
+
+  LivenessSuccessResult(
+    this.valid,
+    this.cause,
+    this.codId,
+    this.protocol,
+    this.scanResultBlob,
+  );
 }
